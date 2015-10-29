5c16258710cd715e1442895c30a7075d2ba9c3d3
Alesary <alesary@alesarys-imac.local>
2015-10-29 16:01:07 +0800

    Initial Commit

---
+++ DouYU/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DouYU
+//
+//  Created by Alesary on 15/10/29.
+//  Copyright © 2015年 Alesary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
